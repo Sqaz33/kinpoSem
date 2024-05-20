a7066270b49f16164dcb39c2d762d26912fe9f48
Matveev Stepan Andreevich <mset321@gmail.com>
2024-05-20 10:51:44 +0300

    добавил папку тестов с cmakelists

--- tests/BigRealNumberTests.cpp
+++ tests/BigRealNumberTests.cpp
@@ -1,3 +1,23 @@
-#include "../include/BigRealNumber.h"
+#include <QtTest/QtTest>
 
-#include <QTest>
+class MyTestClass : public QObject
+{
+    Q_OBJECT
+
+private slots:
+    void test_case1();
+    void test_case2();
+};
+
+void MyTestClass::test_case1()
+{
+    QVERIFY(true);  // Простой тест, который всегда проходит
+}
+
+void MyTestClass::test_case2()
+{
+    QCOMPARE(1 + 1, 2);  // Тест, который проверяет равенство
+}
+
+QTEST_MAIN(MyTestClass)
+#include "BigRealNumberTests.moc"
