86cedc790dde0f8b5909ae63add112cfb695aca6
Matveev Stepan Andreevich <mset321@gmail.com>
2024-05-20 14:24:02 +0300

    закончил с cmakelists для qt тестов, добавил 1  тест для BigRealNumber

--- tests/BigRealNumberTests.cpp
+++ tests/BigRealNumberTests.cpp
@@ -1,23 +1,42 @@
+#include "../include/BigRealNumber.h"
+
+#include <string>
+
 #include <QtTest/QtTest>
 
-class MyTestClass : public QObject
+using std::string;
+
+Q_DECLARE_METATYPE(string);
+
+class BigRealNumberTest : public QObject
 {
     Q_OBJECT
 
 private slots:
-    void test_case1();
-    void test_case2();
+    void fromBRNToString_data();
+    void fromBRNToString();
 };
 
-void MyTestClass::test_case1()
-{
-    QVERIFY(true);  // Простой тест, который всегда проходит
+void BigRealNumberTest::fromBRNToString_data() {
+    QTest::addColumn<string>("toBrn");
+    QTest::addColumn<string>("fromBrn");
+
+    string s1("5.0");
+    string s2("5.0");
+    QTest::addRow("OrdinaryTest") << s1 << s2;
 }
 
-void MyTestClass::test_case2()
-{
-    QCOMPARE(1 + 1, 2);  // Тест, который проверяет равенство
+void BigRealNumberTest::fromBRNToString() {
+    qDebug() << "Fetching test data...";
+    QFETCH(string, toBrn);
+    QFETCH(string, fromBrn);
+
+    BigRealNumber t = BigRealNumber::fromStdString(toBrn);
+
+    // int i = toBrn + 1;
+    QCOMPARE(t.toStdString(), fromBrn);
 }
 
-QTEST_MAIN(MyTestClass)
+QTEST_MAIN(BigRealNumberTest)
+
 #include "BigRealNumberTests.moc"
