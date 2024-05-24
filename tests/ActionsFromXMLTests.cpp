210c1610681b0470c471129e5afbe2c7dc0c2a73
Степан Матвеев <Matveev Stepan>
2024-05-24 12:46:57 +0300

    добавил ActionFromXMLTests.cpp, файл с кодом тестирования ActionFromXML

---
+++ tests/ActionsFromXMLTests.cpp
@@ -0,0 +1,51 @@
+#include "ActionsFromXMLTests.h"
+
+void ActionFromXMLTests::tests_data() {
+    QTest::addColumn<QString>("path");
+    QTest::addColumn<QList<Action>>("actions");
+    QTest::addColumn<QList<int>>("actionsNumb");
+    
+    QTest::addColumn<bool>("isError");
+
+    QTest::addRow("simple_case") << "simple_case_test.xml" 
+                                 <<  QList<Action>{ 
+                                        Action(
+                                            MUL, 
+                                            &BigRealNumber::fromStdString("2.5"),
+                                            &BigRealNumber::fromStdString("3.7")
+                                        ),
+                                        Action(
+                                            FACT, 
+                                            &BigRealNumber::fromStdString("5.0"),
+                                        )
+                                    }
+                                 << QList<int>{1, 2}
+                                 << false;
+    }
+
+
+void ActionFromXMLTests::tests() {
+    QFETCH(QString, path);
+    QFETCH(QList<Action>, actions);
+    QFETCH(QList<int>, actionsNumb);
+    QFETCH(bool, isError);
+    QList<ActionError> errorList;
+
+    // bool isError = false;
+    // try {
+        ActionsFromXML reader(path.toStdString(), &errorList);
+    // }
+
+    const QHash<int, Action*>* XMLActions = reader.getActions();
+    QList<int> keys = XMLActions->keys();
+
+    QCOMPARE(actionsNumb.length(), keys.length());
+    for (int i = 0; i < actionsNumb.length(); i++) {
+        QCOMPARE(actionsNumb.at(i), keys.at(i));
+        QCOMPARE(actions.at(i), XMLActions->value(keys.at(i)));
+
+    }
+
+}
+
+#include "ActionsFromXMLTests.moc"
